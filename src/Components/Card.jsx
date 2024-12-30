6242569e4df65d39d037d2924da5e88a1d7bfed4
aminetiouk <tiouk.amine@gmail.com>
2024-12-30 22:00:08 +0100

    feat: implement count buttons

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,18 +1,20 @@
+import { useState } from 'react';
 import Count from './Count';
 import ResetButton from './ResetButton';
 import Title from './Title';
-import ButtonContainer from './ButtonContainer';
 import CountButton from './CountButton';
+import ButtonContainer from './ButtonContainer';
 
 export default function Card() {
+  const [count, setCount] = useState(0);
   return (
     <div className="card">
       <Title />
-      <Count count={0} />
-      <ResetButton />
+      <Count count={count} />
+      <ResetButton setCount={setCount} />
       <ButtonContainer>
-        <CountButton type="minus" />
-        <CountButton type="plus" />
+        <CountButton type="minus" setCount={setCount} />
+        <CountButton type="plus" setCount={setCount} />
       </ButtonContainer>
     </div>
   );
