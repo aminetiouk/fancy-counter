ec9d8ef6467bf7e9fd8aa951a26378f551337aca
aminetiouk <tiouk.amine@gmail.com>
2024-12-30 21:58:44 +0100

    feat: implement incr and decr a counter

--- src/Components/CountButton.jsx
+++ src/Components/CountButton.jsx
@@ -1,10 +1,31 @@
-import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+import { MinusIcon, PlusIcon } from '@radix-ui/react-icons';
 
-export default function CountButton() {
+export default function CountButton({ setCount, type }) {
+  const handleClick = () => {
+    setCount(c => {
+      if (type === 'minus') {
+        const newCount = c - 1;
+        if (newCount < 10) {
+          return 0;
+        }
+        return newCount;
+      } else {
+        const newCount = c + 1;
+        if (newCount > 10) {
+          return 10;
+        }
+        return newCount;
+      }
+    });
+  };
+
   return (
-    <button className="count-btn">
-      <MinusIcon className="count-btn-icon"></MinusIcon>
-      <PlusIcon className="count-btn-icon"></PlusIcon>
-    </button>
-  )
+      <button className="count-btn" onClick={handleClick}>
+        {type === 'minus' ? (
+          <MinusIcon className="count-btn-icon" />
+        ) : (
+          <PlusIcon className="count-btn-icon" />
+        )}
+      </button>
+  );
 }
