2acf174fc8ed1b57e653dc5eefbcad4fc2e6617a
aminetiouk <tiouk.amine@gmail.com>
2025-01-03 23:12:56 +0100

    feat: add limit title

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,7 +1,13 @@
-export default function Title() {
+export default function Title({ locked }) {
   return (
-    <div>
-      <h1 className="title">Fancy Counter</h1>
-    </div>
+    <h1 className="title">
+      {locked ? (
+        <span>
+          Limit! Buy <b>Pro</b> for &gt; 10
+        </span>
+      ) : (
+        'Fancy Counter'
+      )}
+    </h1>
   );
 }
